594f6942cdc96ddb6e729d9a32798cd67a6891dd
Emmanuel Benazera <beniz@droidnik.fr>
2016-08-07 15:26:23 +0200

    sparse slicing layer cleanup

--- src/caffe/layers/sparse_slice_layer.cpp
+++ src/caffe/layers/sparse_slice_layer.cpp
@@ -22,7 +22,6 @@ void SparseSliceLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void SparseSliceLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 				      const vector<Blob<Dtype>*>& top) {
-  //const int num_axes = bottom[0]->num_axes();
   const SliceParameter& slice_param = this->layer_param_.slice_param();
   slice_axis_ = bottom[0]->CanonicalAxisIndex(slice_param.axis());
   vector<int> top_shape = bottom[0]->shape();
@@ -156,22 +155,7 @@ void SparseSliceLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   /*template <typename Dtype>
 void SparseSliceLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
 					   const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-  if (!propagate_down[0] || top.size() == 1) { return; }
-  int offset_slice_axis = 0;
-  Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
-  const int bottom_slice_axis = bottom[0]->shape(slice_axis_);
-  for (int i = 0; i < top.size(); ++i) {
-    const Dtype* top_diff = top[i]->cpu_diff();
-    const int top_slice_axis = top[i]->shape(slice_axis_);
-    for (int n = 0; n < num_slices_; ++n) {
-      const int top_offset = n * top_slice_axis * slice_size_;
-      const int bottom_offset =
-          (n * bottom_slice_axis + offset_slice_axis) * slice_size_;
-      caffe_copy(top_slice_axis * slice_size_,
-          top_diff + top_offset, bottom_diff + bottom_offset);
-    }
-    offset_slice_axis += top_slice_axis;
-  }
+					  }
   }*/
 
 #ifdef CPU_ONLY
