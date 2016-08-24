1f9f533f3a3f8985f5b0ae8378d3a581eed812cd
Emmanuel Benazera <beniz@droidnik.fr>
2016-08-24 15:51:22 +0200

    cross entropy layer unit tests + remove backward, since sigmoid cross entropy should be used in this case

--- include/caffe/layers/cross_entropy.hpp
+++ include/caffe/layers/cross_entropy.hpp
@@ -25,9 +25,15 @@ namespace caffe {
     virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
 			     const vector<Blob<Dtype>*>& top);
     virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-			      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+			      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
+    {
+      CAFFE_NOT_IMPLEMENTED;
+    }
     virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-			      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+			      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
+    {
+      CAFFE_NOT_IMPLEMENTED;
+    }
 
     int outer_num_;
     int inner_num_;
--- src/caffe/layers/cross_entropy.cpp
+++ src/caffe/layers/cross_entropy.cpp
@@ -23,35 +23,9 @@ namespace caffe {
 	loss -= input_data[j] * (target[j] - (input_data[j] >= 0)) -
 	  log(1 + exp(input_data[j] - 2 * input_data[j] * (input_data[j] >= 0)));
       }
-      top[0]->mutable_cpu_data()[i] = loss;
+      top[0]->mutable_cpu_data()[i] = loss / num;
     }
   }
-
-  template <typename Dtype>
-  void CrossEntropyLayer<Dtype>::Backward_cpu(
-	  const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down,
-	  const vector<Blob<Dtype>*>& bottom) {
-    if (propagate_down[1]) {
-      LOG(FATAL) << this->type()
-		 << " Layer cannot backpropagate to label inputs.";
-    }
-    if (propagate_down[0]) {
-      // First, compute the diff
-      const int count = bottom[0]->count();
-      const int num = bottom[0]->num();
-      const Dtype* output_data = bottom[0]->cpu_data();
-      const Dtype* target = bottom[1]->cpu_data();
-      Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
-      caffe_sub(count, output_data, target, bottom_diff);
-      // Scale down gradient
-      const Dtype loss_weight = top[0]->cpu_diff()[0];
-      caffe_scal(count, loss_weight / num, bottom_diff);
-    }
-  }
-
-#ifdef CPU_ONLY
-  STUB_GPU_BACKWARD(CrossEntropyLayer, Backward);
-#endif
 
 INSTANTIATE_CLASS(CrossEntropyLayer);
 REGISTER_LAYER_CLASS(CrossEntropy);
